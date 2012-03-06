e100e1418dac1b6cf0626aff4308cd9db5656e6b
jrpence <jrpence@ufl.edu>
2012-03-06 18:13:18 -0500

    added some base files and package structure

---
+++ VIVOHarvesterSoapListener/src/main/java/org/vivoweb/harvester/soap/SoapListener.java
@@ -0,0 +1,13 @@
+package org.vivoweb.harvester.soap;
+
+public class SoapListener {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
